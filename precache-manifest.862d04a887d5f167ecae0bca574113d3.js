28691bbbc58126bf52416e89a87a56319c979a5b
Nishaojha <its.nishaojha@gmail.com>
2022-11-15 19:05:38 +0530

    Updates

---
+++ precache-manifest.862d04a887d5f167ecae0bca574113d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5243a9890c17e193b507",
+    "url": "/TextUtils/static/css/main.218b2275.chunk.css"
+  },
+  {
+    "revision": "5243a9890c17e193b507",
+    "url": "/TextUtils/static/js/main.5243a989.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/TextUtils/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "53dde5e360e1493de03f",
+    "url": "/TextUtils/static/js/2.53dde5e3.chunk.js"
+  },
+  {
+    "revision": "423d32646782b88757ba",
+    "url": "/TextUtils/static/js/runtime~main.423d3264.js"
+  },
+  {
+    "revision": "fede4dc2be0cb5ef2f501e796b7a3df2",
+    "url": "/TextUtils/index.html"
+  }
+];
