836c3c6cc94dcb6e469e3981f5c6ad8e3c34184d
Nishaojha <its.nishaojha@gmail.com>
2022-11-15 14:10:36 +0530

    added some more to alert.js

--- src/App.js
+++ src/App.js
@@ -7,26 +7,36 @@ import Alert from './components/Alert';
 
 function App() {
   var [mode, setMode] = useState('dark'); //whether dark mode is enabled or not
-  const [alert, setAlert] = useState(null); //whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null); 
   
-  
+  const showAlert =(message,type)=> { //to show alert messages
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() =>{
+      setAlert(null)
+    },1500)
+  }
   const toggleMode = () => {
     if(mode === 'dark'){
       setMode ('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success")
     }
     else{
       setMode ('dark')
       document.body.style.backgroundColor = '#272c30';
+      showAlert("Dark mode has been enabled", "success")
     }
   }
   return (
     <>
       
       <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode }/> 
-      <Alert alert="This is alert"/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading = "Enter your text here!" mode={mode}/>
+        <TextForm showAlert={showAlert} heading = "Enter your text here!" mode={mode}/>
         {/* <About /> */}
       </div>
       { mode === 'dark'?document.body.style.backgroundColor = '#272c30' : document.body.style.backgroundColor = 'white'}
